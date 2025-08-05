092c8d61de514bd2e20b85f9db1cb9783cd6a45d
vara07 <81780888+vara07@users.noreply.github.com>
2025-08-05 19:59:31 +1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
+
+function openModal040825() {
+  var modal = document.getElementById("modal040825");
+  var img = document.querySelector(".blog-img-040825");
+  var modalImg = document.getElementById("modalImage040825");
+  modal.style.display = "block";
+  modalImg.src = img.src;
+}
+
+function closeModal040825() {
+  document.getElementById("modal040825").style.display = "none";
+}
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelectorAll('.note').forEach(note => {
     note.addEventListener('click', () => {
